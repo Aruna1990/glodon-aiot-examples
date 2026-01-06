455ef7058b39f0c2dc9125f6be72412ec71be4a6
阿如娜 <arn@glodon.com>
2026-01-06 15:04:59 +0800

    fix: 导出组件

--- src/chat-app-sdk-example/components/index.ts
+++ src/chat-app-sdk-example/components/index.ts
@@ -1,3 +1,5 @@
 export { SearchResultList } from './search-result-list';
 export { KnowledgeReferenceList } from './knowledge-reference-list';
 export { MergedReferenceList } from './merged-reference-list';
+export { CustomJsonItem } from './CustomJsonItem';
+export { CustomContentBox } from './CustomContentBox';
